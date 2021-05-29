494b08039c48b13d155c9fd0caf70467c0e8df42
ecleaver <37559968+ecleaver@users.noreply.github.com>
2021-05-29 14:45:33 -0400

    Updated

--- index.js
+++ index.js
@@ -12,14 +12,17 @@ const countGoodTriplets = (arr, a, b, c) => {
   let count = 0;
   for(let i = 0; i < arr.length; i++) {
    for(let j = 1; j < arr.length; j++) {
-    if(0 <=i && i < j && j < k && k < arr.length) {
-      if(Math.abs(arr[i] - arr[j] <= a) && Math.abs(arr[j] - arr[k] <= b) && Math.abs(arr[i] - arr[k] <= c)) {
-        count++;
+     for(let k = 2; k < arr.length; k++) {
+      if(0 <=i && i < j && j < k && k < arr.length) {
+        if(Math.abs(arr[i] - arr[j]) <= a && Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c){
+              count++;
+        } 
+        }
       }
-    }
-   }
+     }
   }
-  return count;  
+   return count;  
+}
   // Where |x| denotes the absolute value of x.
   // Return the number of good triplets.
   // Example 1:
@@ -30,6 +33,5 @@ const countGoodTriplets = (arr, a, b, c) => {
   // Input: arr = [1,1,2,2,3], a = 0, b = 0, c = 1
   // Output: 0
   // Explanation: No triplet satisfies all conditions.
-};
 console.log(countGoodTriplets(arr, a, b, c))
 console.log(countGoodTriplets([1,1,2,2,3], 0, 0, 1))
